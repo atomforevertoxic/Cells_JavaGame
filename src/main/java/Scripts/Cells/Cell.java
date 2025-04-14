d7d1ef1310dd5aa249f54a8e9ead30222f273eaa
Shatalov Ivan <139710109+atomforevertoxic@users.noreply.github.com>
2025-04-14 21:03:38 +0300

    Реализован класс Cell

--- src/main/java/Scripts/Cells/Cell.java
+++ src/main/java/Scripts/Cells/Cell.java
@@ -1,4 +1,27 @@
 package Scripts.Cells;
 
-public class Cell {
+public class Cell
+{
+    private Key _key;
+
+    public boolean SetKey(Key key)
+    {
+        if (_key!=null) return false;
+
+        _key = key;
+        return true;
+    }
+
+    public boolean DeleteKey()
+    {
+        if (_key==null) return false;
+
+        _key=null;
+        return true;
+    }
+
+    public Key GetKey()
+    {
+        return _key;
+    }
 }
