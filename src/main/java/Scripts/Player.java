95516fd7e84714c0303e59730f5b8e7569be01b8
Shatalov Ivan <139710109+atomforevertoxic@users.noreply.github.com>
2025-04-14 21:08:23 +0300

    Реализован класс игрока (с заглушками)

--- src/main/java/Scripts/Player.java
+++ src/main/java/Scripts/Player.java
@@ -1,4 +1,43 @@
 package Scripts;
 
-public class Player {
+import Scripts.Cells.Cell;
+import Scripts.Cells.Key;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Player
+{
+    private Cell _cell;
+
+    private List<Key> _keys = new ArrayList<Key>();
+
+
+    public void SetCell(Cell cell)
+    {
+        _cell = cell;
+    }
+
+    public Cell GetCell()
+    {
+        return _cell;
+    }
+
+    public boolean CanMove(Cell cell)
+    {
+        return true;
+    }
+
+    public void TakeKey(Key key)
+    {
+        _keys.add(key);
+    }
+
+    public List<Key> GetKeys()
+    {
+        return _keys;
+    }
+
+
+
 }
